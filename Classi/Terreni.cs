0d79b2000a00f6a70986d7737d8633f0ca82be52
Corsi.20188 <Corsi.20188@E2-9PCWS2>
2023-12-05 13:19:42 +0100

    classe Attici

--- Classi/Terreni.cs
+++ Classi/Terreni.cs
@@ -6,7 +6,50 @@ using System.Threading.Tasks;
 
 namespace Classi
 {
-    public class Terreni : Immboi
+    public class Terreni : Immobili
     {
+        // attributi
+        public bool _fabbricabile;
+
+        // metodi accessor
+        public bool Fabbricabile
+        {
+            get { return _fabbricabile;}
+            set { _fabbricabile = value;}
+        }
+
+        // costruttore
+        public Terreni(double _superficie, int _particella, int _categoria, bool _fabbricabile) : base(_superficie, _particella, _categoria)
+        {
+            Fabbricabile = _fabbricabile;
+        }
+
+        // metodi
+        public double RenditaTerreni()
+        {
+            double renditaTerreni;
+            double renditaBase = base.RenditaBase();
+            if(Fabbricabile)
+            {
+                renditaTerreni = (renditaBase * 30) / 100;
+                return renditaBase += renditaTerreni;
+            }
+            else
+            {
+                renditaTerreni = (renditaBase * 10) / 100;
+                return renditaBase -= renditaTerreni;
+            }
+        }
+        public override string ToString()
+        {
+            return $"Superficie: {Superficie}, Particella: {Particella}, Categoria: {Categoria}, Fabbricabile: {Fabbricabile}";
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+            Terreni terreni2 = (Terreni)obj;
+            return Fabbricabile == terreni2.Fabbricabile;
+        }
     }
 }
