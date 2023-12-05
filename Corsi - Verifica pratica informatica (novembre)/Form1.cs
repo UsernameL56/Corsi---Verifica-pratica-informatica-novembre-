f525291e8f0bdeebb6703b6722edfebbba0e7eae
Corsi.20188 <Corsi.20188@E2-9PCWS2>
2023-12-05 12:11:49 +0100

    Aggiungere i file di progetto.

---
+++ Corsi - Verifica pratica informatica (novembre)/Form1.cs
@@ -0,0 +1,10 @@
+namespace Corsi___Verifica_pratica_informatica__novembre_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
